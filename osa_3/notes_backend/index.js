5cdb0c753e4125af4a30648526d7e01e0c7c4820
matiasberglund <matias.berglund@hotmail.com>
2023-05-19 14:35:22 +0300

    dotenv notes_backend

--- osa_3/notes_backend/index.js
+++ osa_3/notes_backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const Note = require('./models/note')
@@ -49,45 +50,29 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
-
-    response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -99,7 +84,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
